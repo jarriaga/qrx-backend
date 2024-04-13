a18cefc6c47b15f3abaa2728ae3d6fb6aa42c524
Jesus Barron <jarriagabarron@gmail.com>
2024-04-13 00:57:24 -0500

    configuring prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuserModule } from './quser/quser.module';
-
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [QuserModule],
+  imports: [
+    QuserModule,
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
