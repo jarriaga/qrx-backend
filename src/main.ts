bc33daa3495e67800e3551721b8d480a4889a265
Jesus Barron <jarriagabarron@gmail.com>
2024-11-09 03:11:56 -0600

    main

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,24 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+    try {
+        const app = await NestFactory.create(AppModule);
+        app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-    const config = new DocumentBuilder()
-        .addBearerAuth()
-        .setTitle('API test')
-        .setDescription('API test')
-        .setVersion('1.0')
-        .build();
+        const config = new DocumentBuilder()
+            .addBearerAuth()
+            .setTitle('API test')
+            .setDescription('API test')
+            .setVersion('1.0')
+            .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+        const document = SwaggerModule.createDocument(app, config);
+        SwaggerModule.setup('api', app, document);
 
-    await app.listen(3001);
+        await app.listen(3001);
+    } catch (error) {
+        console.log('Application failed to start:', error);
+        process.exit(1);
+    }
 }
 bootstrap();
