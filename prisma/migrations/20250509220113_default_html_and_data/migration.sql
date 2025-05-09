47e45f64bc447328752f90bc455751fda1c1b681
Jesus Barron <jarriagabarron@gmail.com>
2025-05-09 17:01:54 -0500

    add default html and data to qrcode

---
+++ prisma/migrations/20250509220113_default_html_and_data/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Qrcode" ALTER COLUMN "html" SET DEFAULT 'Hello World',
+ALTER COLUMN "data" SET DEFAULT '{"background":"linear-gradient(to bottom, #0b1480 0%, #020638 100%)"}';
